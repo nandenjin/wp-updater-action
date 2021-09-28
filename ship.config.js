9d447188d5684f06610b72cb659b284be2af4796
Kazumi Inada <hello@nandenjin.com>
2021-09-28 20:38:50 +0900

    chore: Create short-handed tag after publish

--- ship.config.js
+++ ship.config.js
@@ -4,4 +4,10 @@ module.exports = {
     exec('git add dist')
   },
   publishCommand: () => 'echo NOOP',
+  afterPublish({ exec, version }) {
+    // Create and push short-handed tag
+    const shortName = 'v' + version.replace(/\..+$/, '')
+    exec(`git tag -f ${shortName}`)
+    exec(`git push -f origin ${shortName}`)
+  },
 }
